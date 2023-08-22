63f1b7e05705a722f3a9c87ed4dcf7a0656624c2
Viserion-7 <harigovindbiju@gmail.com>
2023-08-22 21:43:11 +0530

    done

--- backend/index.js
+++ backend/index.js
@@ -5,9 +5,20 @@ const app = express();
 app.use(express.json());
 app.use(cors({ origin: true }));
 
+const axios = require("axios");
 app.post("/authenticate", async (req, res) => {
   const { username } = req.body;
-  return res.json({ username: username, secret: "sha256..." });
+  // Get or create user on Chat Engine!
+  try {
+    const r = await axios.put(
+      "https://api.chatengine.io/users/",
+      { username: username, secret: username, first_name: username },
+      { headers: { "Private-Key": "00103972-e6a0-46f2-b746-1b68e0df3970"} }
+    );
+    return res.status(r.status).json(r.data);
+  } catch (e) {
+    return res.status(e.response.status).json(e.response.data);
+  }
 });
 
 app.listen(3001);
